4684cb57fa220f461e2bab4b72eb1c015cb68cec
Marcus Leong <marcusleongjx@gmail.com>
2024-07-04 17:24:56 +0800

    Add html elements and js code for adding content

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -8,5 +10,17 @@ function Book(title, author, pages, read) {
     };
 }
 
+function addBookToLibrary() {
+    myLibrary.push(this.title)
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
 console.log(theHobbit.info());
+
+const container = document.querySelector(".container");
+
+const book = document.createElement("div");
+book.classList.add("book");
+book.textContent = 'Harry Potterer';
+console.log(book);
+container.appendChild(book);
